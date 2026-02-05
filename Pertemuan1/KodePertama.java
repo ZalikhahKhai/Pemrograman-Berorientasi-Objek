73b87cea650f062a684705b7f7cc1987f31c84cd
ZalikhahKhai <chachakhairunnisa21@gmail.com>
2026-02-05 14:44:05 +0700

    KodePertama.java

--- Pertemuan1/KodePertama.java
+++ Pertemuan1/KodePertama.java
@@ -1,3 +1,9 @@
+/*
+Nama: Zalikhah Khairunnisa
+NIM: D1041241018
+Tanggal: 05--2-2026 
+*/
+package Pertemuan1;
 public class KodePertama {
     public static void main(String[] args) {
         System.out.println("Halo, Pemrograman Berorientasi Objek!");

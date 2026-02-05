d3aace1277d69cfa979f6ad86e219ad769778545
ZalikhahKhai <chachakhairunnisa21@gmail.com>
2026-02-05 14:46:53 +0700

    Add files via upload

---
+++ Pertemuan1/KodePertama_Latihan.java
@@ -0,0 +1,8 @@
+public class KodePertama_Latihan {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Zalikhah Khairunnisa");
+        System.out.println("NIM     : D1041241018");
+        System.out.println("Semester: 4");
+        System.out.println("Prodi   : Informatika");
+    }
+}

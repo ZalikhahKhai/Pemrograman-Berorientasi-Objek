f7607b742d18bcf56b17608432b90480ba72ab0c
ZalikhahKhai <chachakhairunnisa21@gmail.com>
2026-02-05 14:30:15 +0700

    KodePertama

---
+++ Pertemuan1/KodePertama.java
@@ -0,0 +1,6 @@
+public class KodePertama {
+    public static void main(String[] args) {
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalah program Java pertama saya.");
+    }
+}
